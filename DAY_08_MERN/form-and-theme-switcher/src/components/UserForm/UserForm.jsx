51cce5fbac3b1c5a251fe60a706d87fd2468939e
pradipk1 <pradipshukla12699@gmail.com>
2024-04-18 21:25:57 +0530

    day08 mern problem1 form in react done!

---
+++ DAY_08_MERN/form-and-theme-switcher/src/components/UserForm/UserForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './UserForm.css';
+import { useState } from 'react';
+import { useRef } from 'react';
+
+function UserForm() {
+    const [name, setName] = useState('');
+    const emailRef =useRef();
+    const [valid, setValid] = useState(false);
+
+    const handleForm = (e) => {
+        e.preventDefault();
+        if(name.length < 3) {
+            setValid(true);
+        }
+        console.log(name);
+        console.log(emailRef.current.value);
+    }
+
+  return (
+    <div className='userFormMain'>
+        <h2>User Form</h2>
+        <form onSubmit={handleForm}>
+            <div>
+                <label>Name</label>
+                <input type="text" placeholder='Enter Name' 
+                    onChange={(e) => {
+                        setName(e.target.value);
+                        if(e.target.value.length < 3) {
+                            setValid(true);
+                        } else {
+                            setValid(false);
+                        }
+                    }}
+                />
+                {valid && <p className='validationMessage'>Must have atleast 3 characters.</p>}
+            </div>
+
+            <div>
+                <label>Email</label>
+                <input type="email" placeholder='Enter Email' ref={emailRef}/>
+            </div>
+
+            <input type="submit" disabled={name.length < 3}/>
+        </form>
+    </div>
+  )
+}
+
+export default UserForm;
